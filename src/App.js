b97e63c23b787cf51a340df6705d9ef79395f6ef
Julio <julio.chazari@gmail.com>
2020-11-18 20:12:13 -0600

    Adding settings nav item

--- src/App.js
+++ src/App.js
@@ -49,9 +49,15 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            {isAuthenticated
-              ? <NavItem onClick={handleLogout}>Logout</NavItem>
-              : <>
+            {isAuthenticated ? (
+                <>
+                  <LinkContainer to="/settings">
+                    <NavItem>Settings</NavItem>
+                  </LinkContainer>
+                  <NavItem onClick={handleLogout}>Logout</NavItem>
+                </>
+              ) : (
+                <>
                   <LinkContainer to="/signup">
                     <NavItem>Signup</NavItem>
                   </LinkContainer>
@@ -59,7 +65,7 @@ function App() {
                     <NavItem>Login</NavItem>
                   </LinkContainer>
                 </>
-            }
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
